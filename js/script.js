955c322b9ccf59f070198a6c8b647f1ff2af575e
OmPimpale <ompimpale2003@gmail.com>
2025-01-14 16:25:02 +0530

    crose chruch site created and updated

---
+++ js/script.js
@@ -0,0 +1,137 @@
+// for div heading
+let color1 = () => {
+    let color1 = document.getElementById("herodivhead1");
+    color1.style.color = "rgb(201, 47, 47)";
+    color1.style.transition = "all 0.5s";
+}
+
+let removeColor1 = () => {
+    let icon = document.getElementById("herodivhead1");
+    icon.style.color = "rgb(31, 31, 44)";
+}
+
+let color2 = () => {
+    let color1 = document.getElementById("herodivhead2");
+    color1.style.color = "rgb(201, 47, 47)";
+    color1.style.transition = "all 0.4s";
+}
+
+let removeColor2 = () => {
+    let icon = document.getElementById("herodivhead2");
+    icon.style.color = "rgb(31, 31, 44)";
+}
+
+let color3 = () => {
+    let color1 = document.getElementById("herodivhead3");
+    color1.style.color = "rgb(201, 47, 47)";
+    color1.style.transition = "all 0.4s";
+}
+
+let removeColor3 = () => {
+    let icon = document.getElementById("herodivhead3");
+    icon.style.color = "rgb(31, 31, 44)";
+}
+
+// for read more in herodiv
+let heroAnchorIcon1 = () => {
+    let icon = document.getElementById("herodivicon1");
+    icon.style.marginLeft = "10px";
+    icon.style.color = "rgb(201, 47, 47)";
+    icon.style.transition = "all 0.8s";
+}
+
+let removemargin1 = () => {
+    let icon = document.getElementById("herodivicon1");
+    icon.style.marginLeft = "0px";
+    icon.style.color = "#959595";
+}
+
+let heroAnchorIcon2 = () => {
+    let icon = document.getElementById("herodivicon2");
+    icon.style.marginLeft = "10px";
+    icon.style.color = "rgb(201, 47, 47)";
+    icon.style.transition = "all 0.8s";
+}
+
+let removemargin2 = () => {
+    let icon = document.getElementById("herodivicon2");
+    icon.style.marginLeft = "0px";
+    icon.style.color = "#959595";
+}
+
+let heroAnchorIcon3 = () => {
+    let icon = document.getElementById("herodivicon3");
+    icon.style.marginLeft = "10px";
+    icon.style.color = "rgb(201, 47, 47)";
+    icon.style.transition = "all 0.8s";
+}
+
+let removemargin3 = () => {
+    let icon = document.getElementById("herodivicon3");
+    icon.style.marginLeft = "0px";
+    icon.style.color = "#959595";
+}
+
+// sermon
+let giveMargin1 = () => {
+    let top = document.getElementById("sermonmargindiv1");
+    let span = document.getElementById("sermonspan1");
+    top.style.marginTop = "-70px";
+    top.style.marginBottom = "49px";
+    top.style.transition = "all 0.3s";
+    top.style.backgroundColor = "rgb(201, 47, 47)";
+    top.style.color = "white";
+    span.style.color = "white";
+}
+
+let margin1 = () => {
+    let top = document.getElementById("sermonmargindiv1");
+    let span = document.getElementById("sermonspan1");
+    top.style.marginTop = "-45px";
+    top.style.marginBottom = "0px";
+    top.style.backgroundColor = "white";
+    top.style.color = "rgb(31, 31, 44)";
+    span.style.color = "#959595";
+}
+
+let giveMargin2 = () => {
+    let top = document.getElementById("sermonmargindiv2");
+    let span = document.getElementById("sermonspan2");
+    top.style.marginTop = "-70px";
+    top.style.marginBottom = "49px";
+    top.style.transition = "all 0.3s";
+    top.style.backgroundColor = "rgb(201, 47, 47)";
+    top.style.color = "white";
+    span.style.color = "white";
+}
+
+let margin2 = () => {
+    let top = document.getElementById("sermonmargindiv2");
+    let span = document.getElementById("sermonspan2");
+    top.style.marginTop = "-45px";
+    top.style.marginBottom = "0px";
+    top.style.backgroundColor = "white";
+    top.style.color = "rgb(31, 31, 44)";
+    span.style.color = "#959595";
+}
+
+let giveMargin3 = () => {
+    let top = document.getElementById("sermonmargindiv3");
+    let span = document.getElementById("sermonspan3");
+    top.style.marginTop = "-70px";
+    top.style.marginBottom = "49px";
+    top.style.transition = "all 0.3s";
+    top.style.backgroundColor = "rgb(201, 47, 47)";
+    top.style.color = "white";
+    span.style.color = "white";
+}
+
+let margin3 = () => {
+    let top = document.getElementById("sermonmargindiv3");
+    let span = document.getElementById("sermonspan3");
+    top.style.marginTop = "-45px";
+    top.style.marginBottom = "0px";
+    top.style.backgroundColor = "white";
+    top.style.color = "rgb(31, 31, 44)";
+    span.style.color = "#959595";
+}
